1d1a85d09a3594167f37d9b65ec15525fe408e57
Eastroots92 <eastroots92@gmail.com>
2018-06-30 23:26:22 +0900

    Add Fragment in MainActivity
    MainActivity에 Fragment 추가

--- app/src/main/java/com/openull/eastroots92/vacation_homework_android/ui/Activity/MainActivity.java
+++ app/src/main/java/com/openull/eastroots92/vacation_homework_android/ui/Activity/MainActivity.java
@@ -1,33 +1,70 @@
 package com.openull.eastroots92.vacation_homework_android.ui.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.FrameLayout;
 
 import com.openull.eastroots92.vacation_homework_android.R;
 import com.openull.eastroots92.vacation_homework_android.presenter.main.MainContract;
+import com.openull.eastroots92.vacation_homework_android.ui.adapter.MainFragmentAdapter;
 import com.openull.eastroots92.vacation_homework_android.ui.adapter.UserInfoAdapter;
+import com.openull.eastroots92.vacation_homework_android.ui.fragments.CalendarFragment;
+import com.openull.eastroots92.vacation_homework_android.ui.fragments.SettingFragment;
+import com.openull.eastroots92.vacation_homework_android.ui.fragments.TimelineFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
-  private RecyclerView recyclerView;
-  private RecyclerView.Adapter userInfoAdapter;
+
+  private CalendarFragment calendarFragment;
+  private SettingFragment settingFragment;
+  private TimelineFragment timelineFragment;
+
+  @BindView(R.id.frameLayout_main)
+  ViewPager mainFrameLayout;
+
+  @BindView(R.id.tabLayout_navBar)
+  TabLayout tabLayout;
+
+  private MainFragmentAdapter mainFragmentAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
     initView();
   }
 
   @Override
   public void initView() {
-    recyclerView = findViewById(R.id.recyclerView_list);
+    calendarFragment = new CalendarFragment();
+    settingFragment = new SettingFragment();
+    timelineFragment = new TimelineFragment();
 
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    userInfoAdapter = new UserInfoAdapter();
+    initViewPager();
+    initTabLayout();
 
-    recyclerView.setAdapter(userInfoAdapter);
+  }
+
+  private void initViewPager() {
+
+    Fragment[] fragments = new Fragment[3];
+    fragments[0] = timelineFragment;
+    fragments[1] = calendarFragment;
+    fragments[2] = settingFragment;
+
+    mainFrameLayout.setAdapter(new MainFragmentAdapter(getSupportFragmentManager(), fragments));
+  }
+
+  private void initTabLayout() {
+    tabLayout.setupWithViewPager(mainFrameLayout);
   }
 }
---
+++ app/src/main/java/com/openull/eastroots92/vacation_homework_android/ui/fragments/TimelineFragment.java
@@ -0,0 +1,27 @@
+package com.openull.eastroots92.vacation_homework_android.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.openull.eastroots92.vacation_homework_android.R;
+
+public class TimelineFragment extends Fragment {
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_timeline, container, false);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    // Code
+  }
+}
