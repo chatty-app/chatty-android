952b37949f3534f235a5c0d45ddf126d2a0ce9cb
KimYujeong <rladbwjd341@naver.com>
2018-08-16 02:12:55 +0900

    Setting MainHeader
    - MainActivity와CalendarActivity의 View이동기능을 넣는다.
    - Main과 Calendar의 관련 레이아웃을 수정한다.
    
    Fixes https://github.com/chatty-app/chatty-app/issues/30

--- app/src/main/java/com/chatty/android/chattyClient/view/calendar/CalendarActivity.java
+++ app/src/main/java/com/chatty/android/chattyClient/view/calendar/CalendarActivity.java
@@ -1,15 +1,24 @@
 package com.chatty.android.chattyClient.view.calendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.chatty.android.chattyClient.R;
 import com.chatty.android.chattyClient.presenter.calendar.CalendarPresenter;
+import com.chatty.android.chattyClient.view.main.MainActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CalendarActivity extends AppCompatActivity {
   private CalendarPresenter presenter;
+
+  @BindView(R.id.button_timeline_left)
+  public ImageButton backButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +31,20 @@ public class CalendarActivity extends AppCompatActivity {
     ButterKnife.bind(this);
     presenter = new CalendarPresenter(this);
     presenter.construct();
+
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(MainActivity.HEADER_TITLE);
+    TextView textView = findViewById(R.id.textView_timeline_title);
+    textView.setText(message);
   }
 
   public void render() {
+    renderBackButton();
+  }
+
+  private void renderBackButton() {
+    backButton.setOnClickListener(view -> {
+        finish();
+    });
   }
 }
