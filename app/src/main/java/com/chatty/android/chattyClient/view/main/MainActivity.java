94c410109c5962546718524b0816c4ffc6efed05
Kim Yujeong <dbwjd@users.noreply.github.com>
2018-08-16 16:51:31 +0900

    Setting MainHeader
    - MainActivity와CalendarActivity의 View이동기능을 넣는다.
    - Main과 Calendar의 관련 레이아웃을 수정한다.
    
    Fixes https://github.com/chatty-app/chatty-app/issues/30

--- app/src/main/java/com/chatty/android/chattyClient/view/main/MainActivity.java
+++ app/src/main/java/com/chatty/android/chattyClient/view/main/MainActivity.java
@@ -1,15 +1,21 @@
 package com.chatty.android.chattyClient.view.main;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.chatty.android.chattyClient.R;
 import com.chatty.android.chattyClient.presenter.main.MainPresenter;
 import com.chatty.android.chattyClient.presenter.main.TimelineRecyclerViewAdapter;
+import com.chatty.android.chattyClient.view.calendar.CalendarActivity;
+import com.chatty.android.chattyClient.view.setting.SettingActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +24,19 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+  public static final String HEADER_TITLE = "HEADER_TITLE";
+  public static final String CALENDAR = "Calendar";
+  public static final String SETTING = "Setting";
   private MainPresenter presenter;
 
-  @BindView(R.id.floatingActionButton_write)
+  @BindView(R.id.btn_start_chatting)
   public FloatingActionButton writeButton;
+
+  @BindView(R.id.btn_main_header_setting)
+  public ImageButton settingButton;
+
+  @BindView(R.id.btn_main_header_calendar)
+  public ImageButton calenderButton;
 
   @BindView(R.id.recyclerView_timeline)
   public RecyclerView recyclerView;
@@ -45,6 +60,22 @@ public class MainActivity extends AppCompatActivity {
   public void render(View.OnClickListener handleClickWriteButton) {
     renderWriteButton(handleClickWriteButton);
     renderTimeLineView();
+    renderMainHeader();
+  }
+
+  private void renderMainHeader() {
+    calenderButton.setOnClickListener(view -> {
+        Intent intent = new Intent(getApplicationContext(), CalendarActivity.class);
+        String message = CALENDAR;
+        intent.putExtra(HEADER_TITLE, message);
+        startActivity(intent);
+    });
+    settingButton.setOnClickListener(view -> {
+      Intent intent = new Intent(this, SettingActivity.class);
+      String message = SETTING;
+      intent.putExtra(HEADER_TITLE, message);
+      startActivity(intent);
+    });
   }
 
   private void renderTimeLineView() {
